a4e1e8d066f3513792b5668a1310102bb52ab498
Takuya Mukohira <takuya.mk96@gmail.com>
2017-08-18 12:18:45 +0900

    Create Stack class

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 require 'stack'
 
 describe Stack do
+  describe '#initialize' do
+    let(:stack) { Stack.new }
+    subject { stack }
 
+    it { is_expected.to be_kind_of Stack }
+  end
 end
